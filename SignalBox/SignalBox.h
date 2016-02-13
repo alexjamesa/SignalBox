40bbdf2cedccb34e0c5dde5da6ac737a1d97ad13
U3 <U3@u3s-macbook-pro.local>
2016-02-13 06:48:42 -0500

    Initial Commit

---
+++ SignalBox/SignalBox.h
@@ -0,0 +1,19 @@
+//
+//  SignalBox.h
+//  SignalBox
+//
+//  Created by U3 on 160213.
+//  Copyright © 2016 Ten Kettles Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SignalBox.
+FOUNDATION_EXPORT double SignalBoxVersionNumber;
+
+//! Project version string for SignalBox.
+FOUNDATION_EXPORT const unsigned char SignalBoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignalBox/PublicHeader.h>
+
+
